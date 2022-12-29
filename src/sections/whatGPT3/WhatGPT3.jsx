4d6b3c66b574f5fb8085b5b8d66ac02ff110f829
Przemek <owca.przemek@hotmail.com>
2022-12-29 15:44:59 +0100

    Added whatgpt3 section +reusable feature component

--- src/sections/whatGPT3/WhatGPT3.jsx
+++ src/sections/whatGPT3/WhatGPT3.jsx
@@ -1,7 +1,44 @@
 import React from "react";
 import "./whatGPT3.css";
+import Feature from "../../components/feature/Feature";
+
+const data = [
+  {
+    title: "What is GPT-3",
+    text: "We so opinion friends me message as delight. Whole front do of plate heard oh ought. His defective nor convinced residence own. Connection has put impossible own apartments boisterous. At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by.",
+  },
+  {
+    title: "Chatbots",
+    text: "We so opinion friends me message as delight. Whole front do of plate heard oh ought. ",
+  },
+  {
+    title: "Knowledgebase",
+    text: "At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments b",
+  },
+  {
+    title: "Education",
+    text: "At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments b",
+  },
+];
 const WhatGPT3 = () => {
-  return <div>WhatGPT3</div>;
+  return (
+    <div className="gpt3__whatgpt3 section__margin" id="wgpt3">
+      <div className="gpt3__whatgpt3-feature">
+        <Feature title={data[0].title} text={data[0].text} />
+      </div>
+      <div className="gpt3__whatgpt3-heading">
+        <h1 className="gradient__text">
+          The possibilities are beyond your imagination
+        </h1>
+        <p>Explore The Library</p>
+      </div>
+      <div className="gpt3__whatgpt3-features">
+        <Feature title={data[1].title} text={data[1].text} />
+        <Feature title={data[2].title} text={data[2].text} />
+        <Feature title={data[3].title} text={data[3].text} />
+      </div>
+    </div>
+  );
 };
 
 export default WhatGPT3;
