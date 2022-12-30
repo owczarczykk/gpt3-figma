9ef402b601aa63603c303b7f37cd64dd391ed29a
Przemek <owca.przemek@hotmail.com>
2022-12-30 15:03:50 +0100

    added cta section

--- src/components/cta/Cta.jsx
+++ src/components/cta/Cta.jsx
@@ -1,7 +1,17 @@
 import React from "react";
 import "./cta.css";
 const Cta = () => {
-  return <div>Cta</div>;
+  return (
+    <div className="gpt3__cta section__margin">
+      <div className="gpt3__cta-text">
+        <p>Request Early Access to Get Started</p>
+        <h1>Register today & start exploring the endless possiblities.</h1>
+      </div>
+      <div className="gpt3__cta-button">
+        <button type="button">Get Started </button>
+      </div>
+    </div>
+  );
 };
 
 export default Cta;
